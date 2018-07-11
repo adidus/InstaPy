006a328ca44b7d3cbd5ef172cf352db37cda7aec
ZeusFSX <zeusfsx@gmail.com>
2018-07-11 15:28:20 +0300

    fix bug

--- cmdtest.py
+++ cmdtest.py
@@ -7,17 +7,18 @@ insta_password = sys.argv[2]
 # if you want to run this script on a server,
 # simply add nogui=True to the InstaPy() constructor
 
-usernames = ['podoliakk_']
+usernames = []
 #Read instagram_accounts from file
-#with open('instagram_accounts.csv','r') as file:
-#    temp = file.readlines()
-#    for user in temp:
-#        usernames.append(user.replace("\n",""))
+with open('instagram_accounts.csv','r') as file:
+    temp = file.readlines()
+    for user in temp:
+        usernames.append(user.replace("\n",""))
+print(usernames[:10])
 
 session = InstaPy(username=insta_username, password=insta_password, nogui= True)
 session.login()
 #session.follow_by_list(usernames,times = 1,sleep_delay = 4743, interact = False)
-limit_amount = 12000
+limit_amount = 60
 session.accounts_to_json(usernames,limit_amount)
 #session.feeds_to_json()
 
